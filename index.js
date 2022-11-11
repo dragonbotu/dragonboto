e3340111346be53303613580b171acc07832705a
dragonbotu <117978177+dragonbotu@users.noreply.github.com>
2022-11-11 12:44:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -35,21 +35,5 @@ client.on("interactionCreate", async (interaction) => {
      }
     })
 //https://ra3dstudio.com CopyRight Codes
-static void UpdatePresence()
-{
-    DiscordRichPresence discordPresence;
-    memset(&discordPresence, 0, sizeof(discordPresence));
-    discordPresence.state = "Playing Solo";
-    discordPresence.details = "Competitive";
-    discordPresence.startTimestamp = 1507665886;
-    discordPresence.endTimestamp = 1507665886;
-    discordPresence.largeImageText = "Numbani";
-    discordPresence.smallImageText = "Rogue - Level 100";
-    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
-    discordPresence.partySize = 1;
-    discordPresence.partyMax = 5;
-    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
-    Discord_UpdatePresence(&discordPresence);
-}
 ///
 client.login(process.env.token)
