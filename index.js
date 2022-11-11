d809115d56f0b6c74f3856e2fd4bcf47d65b382e
dragonbotu <117978177+dragonbotu@users.noreply.github.com>
2022-11-11 20:56:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,35 +17,6 @@ client.on('message' , message => {
   }  
  });
 //https://ra3dstudio.com CopyRight Codes
-//كود افاتار سلاش كمند | Avatar with slash cmd
-const { MessageEmbed } = require('discord.js');
-module.exports = {
-  name: 'avatar',
-  description: 'Get a member avatar',
-  cooldown: 5,
-  options: [
-    {
-      name: 'user',
-      description: 'the targeted user',
-      type: 'USER'
-    }
-  ],
-  run: async function(client, interaction, args) {
-
-    let user = interaction.guild.members.cache.get(args[0]) || interaction.member;
-
-    let embed = new MessageEmbed()
-      .setColor('BLUE')
-      .setAuthor(interaction.user.tag, interaction.user.avatarURL({ dynamic: true }))
-      .setImage(user.user.displayAvatarURL({ dynamic: true, size: 1024 }))
-      .setTitle(`${user.user.username}`)
-      .setURL(user.user.displayAvatarURL({ dynamic: true, size: 1024 }))
-
-    await interaction.followUp({ embeds: [embed] })
-
-  }
-}
-//https://ra3dstudio.com CopyRight Codes
 ///
 client.login(process.env.token)
 
