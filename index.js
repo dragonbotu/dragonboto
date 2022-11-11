5a0ce5b4ed6a79b298e2a79b191c743f3877b84d
dragonbotu <117978177+dragonbotu@users.noreply.github.com>
2022-11-11 12:36:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//السورس الاساسي لـ دسكورد نسخة 12 | the main source of discord.js 12v
 const Discord = require('discord.js');
 const { Client, Intents,MessageEmbed } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING] });
@@ -6,11 +7,8 @@ client.on("ready", () => {
   console.log(`✅ | Logged as ${client.user.tag}`);
 })
 //////////////////////////////////////
-//
-})   
-//https://ra3dstudio.com CopyRight Codes
 //كود بنق نفس بروبوت القديم | ping code like probot
-client.on('messageCreate' , message => {
+client.on('message' , message => {
   if(message.author.bot) return;
   if(message.content.startsWith("ping")) {
  message.channel.send('Pong...').then((msg) => {
