8e6f5b8f869e4a1c6e22163da4d7fe05834bbcb8
dragonbotu <117978177+dragonbotu@users.noreply.github.com>
2022-11-11 15:28:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const Discord = require('discord.js');
+const { Client, Intents,MessageEmbed } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING] });
+
+client.on("ready", () => {
+  console.log(✅ | Logged as ${client.user.tag});
+})
+//////////////////////////////////////
+'use strict';
+
+/**
+ * A ping pong bot, whenever you send "ping", it replies "pong".
+ */
+
+// Import the discord.js module
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
 const client = new Discord.Client();
 
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('I am ready!');
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
   }
 });
+///
 
-client.login('MTA0MDUzNDY2ODQxNzY5NTc1NQ.G43miR.L65iGDqUI5gB185ofODq16sC8SF9hFaIeVhc78');
+client.login(process.env.token)
